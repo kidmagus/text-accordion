ba6e4ff5f8f8f3ff67f5e939c328016ee19f8366
kidmagus <msamson19@Outlook.com>
2024-04-15 18:44:58 +0800

    update module

--- js/module.js
+++ js/module.js
@@ -1,28 +1,38 @@
 
-const singleBoxes = document.querySelectorAll(".single");
+//Call all the accordion--modifier
+const accordions = document.querySelectorAll(".accordion");
 
-singleBoxes.forEach(box => {
-  box.addEventListener("click", event => {
-    if (box.classList.contains('expanded')) {
-      box.classList.remove("expanded");
-    } else {
-      document.querySelector(".box.expanded")?.classList.remove("expanded");
-      box.classList.add("expanded");
+//For every accordion .. do this
+accordions.forEach((accordion) => {
+  //under accordion , call the class that has .box 
+  const boxes= accordion.querySelectorAll('.box');
+  const isSingleOpen = document.querySelectorAll('.single')
+
+    if (boxes){
+      boxes.forEach((box) => {
+        box.addEventListener("click", () => {
+          box.classList.toggle('active');
+
+        });
+      });
+    } 
+
+
+    if (isSingleOpen) {
+      isSingleOpen.forEach((single) => {
+        single.addEventListener("click", () => {
+        
+          if (single.classList.contains('active')){
+              single.classList.remove('active');
+          } else {
+            document.querySelector(".single.active")?.classList.remove("active");
+            single.classList.add("active");
+          }
+       
+         
+        })
+      })
     }
   });
-});
 
 
-const multipleBoxes = document.querySelectorAll('.multiple');
-
-multipleBoxes.forEach((box) => {
-    const boxInfo = box.querySelector('.box__info')
-
-    if (boxInfo){
-        boxInfo.addEventListener("click", (event) =>{
-        box.classList.toggle('active');
-        });
-
-
-    }
-})
